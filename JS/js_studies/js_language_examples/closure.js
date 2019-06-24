3483a21790746a28aa3ce4e4e63a1ea0fbb013ac
Mia Steinkirch <mia.steinkirch@gmail.com>
2019-06-24 13:04:12 -0700

    first commit

---
+++ JS/js_studies/js_language_examples/closure.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+function User(){
+	var username, password;
+
+	function doLogin(user,pw) {
+		username = user;
+		password = pw;
+
+		// do the rest of the login work
+	}
+
+	var publicAPI = {
+		login: doLogin
+	};
+
+	return publicAPI;
+}
+
+// create a `User` module instance
+var fred = User();
+
+console.log( fred.login( "fred", "12Battery34!" ));
+
